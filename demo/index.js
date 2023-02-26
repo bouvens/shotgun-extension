443c1becaa2715b7982e0d6ce977035d3a62b12d
Alex Demin <bouvens@gmail.com>
2023-02-26 20:52:07 +0300

    refactor: Update initialization timer name

--- demo/index.js
+++ demo/index.js
@@ -1,10 +1,12 @@
 import initialize from './src'
 
+const INITIALIZATION_TIMER_NAME = '🔫 Initialization'
+
 document.addEventListener('readystatechange', (event) => {
   if (event.target.readyState !== 'complete') {
     return
   }
-  console.time('initialization')
+  console.time(INITIALIZATION_TIMER_NAME)
   initialize()
-  console.timeEnd('initialization')
+  console.timeEnd(INITIALIZATION_TIMER_NAME)
 })
